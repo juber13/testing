a8c7493dca0a5f3276e07f761ae3e90434aada27
juber <juberk1010@gmail.com>
2023-11-02 13:02:13 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  // Other Rollup configuration options...
+  build: {
+    rollupOptions: {
+      input: {
+        // main: '/src/main.jsx',
+        main: './src/main.jsx',
+      },
+      output: {
+        // Specify the desired output directory and format
+        dir: './src',
+        format: 'es',
+      },
+      // Exclude HTML files from bundling
+      // exclude: ['/index.html'],
+      exclude: ['./index.html'],
+    },
+  },
+};
